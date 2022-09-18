946891205ef5e8c1e015482936e1edc081affadf
katsuragawa <andre.katsuragawa@gmail.com>
2022-09-18 17:33:52 -0300

    two balls problem hehe

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const jumpSize = Math.floor(Math.sqrt(breaks.length));
+  let i = jumpSize;
+  for (; i <= breaks.length; i += jumpSize) {
+    if (breaks[i]) {
+      break;
+    }
+  }
 
+  i -= jumpSize;
+  for (let j = 0; j < jumpSize && i < breaks.length; j++, i++) {
+    if (breaks[i]) {
+      return i;
+    }
+  }
+
+  return -1;
 }
