0d5348c5737e84a587b5800e9d6725b49bbddfa3
katsuragawa <andre.katsuragawa@gmail.com>
2022-09-23 21:31:26 -0300

    implement curitibanos na pegando bus

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,51 @@
 export default class Queue<T> {
-    public length: number;
+  length: number;
 
-    
+  private head?: Node<T>;
 
-    constructor() {
+  private tail?: Node<T>;
+
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
+
+  enqueue(item: T): void {
+    const node: Node<T> = { value: item };
+    this.length++;
+    if (!this.tail) {
+      this.tail = this.head = node;
+      return;
     }
 
-    enqueue(item: T): void {
+    this.tail.next = node;
+    this.tail = node;
+  }
 
-}
-    deque(): T | undefined {
+  deque(): T | undefined {
+    if (!this.head) {
+      return;
+    }
 
-}
-    peek(): T | undefined {
+    const head = this.head;
+    this.head = this.head.next;
+    head.next = undefined;
 
+    this.length--;
+
+    if (this.length === 0) {
+      this.tail = undefined;
+    }
+
+    return head.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
-}
+
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
