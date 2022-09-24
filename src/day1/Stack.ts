cd9633b9f5da4d493d4d1e0a13fb827b308ba045
katsuragawa <andre.katsuragawa@gmail.com>
2022-09-23 21:41:18 -0300

    keep it a stack bro

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,37 @@
 export default class Stack<T> {
-    public length: number;
+  length: number;
 
-    
+  private head?: Node<T>;
 
-    constructor() {
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
+
+  push(item: T): void {
+    const node: Node<T> = { value: item, next: this.head };
+    this.head = node;
+    this.length++
+  }
+
+  pop(): T | undefined {
+    if (!this.head) {
+      return;
     }
 
-    push(item: T): void {
+    this.length--;
+    const head = this.head;
+    this.head = this.head.next;
+    head.next = undefined;
+    return head.value;
+  }
 
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
-    pop(): T | undefined {
 
-}
-    peek(): T | undefined {
-
-}
-}
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
