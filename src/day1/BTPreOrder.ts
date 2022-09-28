dd774fb8a06cbaa9c70b1323193d0fc648dc1e79
katsuragawa <andre.katsuragawa@gmail.com>
2022-09-27 21:21:04 -0300

    pre order BT traversal

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,15 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(node: BinaryNode<number> | null, path: number[]): void {
+  if (!node) {
+    return;
+  }
 
+  path.push(node.value);
+  walk(node.left, path);
+  walk(node.right, path);
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+  const path: number[] = [];
+  walk(head, path);
+  return path;
 }
