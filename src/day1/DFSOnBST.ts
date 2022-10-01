d3a2e1576c18e2f4ceb73d8b429f287a76e885e2
katsuragawa <andre.katsuragawa@gmail.com>
2022-10-01 19:32:50 -0300

    is not BTS

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,19 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function find(node: BinaryNode<number> | null, needle: number): boolean {
+  if (!node) {
+    return false;
+  }
 
+  if (needle === node.value) {
+    return true;
+  }
+
+  if (needle < node.value) {
+    return find(node.left, needle);
+  }
+
+  return find(node.right, needle);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+  return find(head, needle);
 }
