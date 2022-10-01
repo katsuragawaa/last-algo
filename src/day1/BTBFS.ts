060f18d4e0b085524ddc9e1910cde69e8129754c
katsuragawa <andre.katsuragawa@gmail.com>
2022-10-01 12:57:54 -0300

    implement breadth first

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,27 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+import Queue from './Queue';
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const queue = new Queue<BinaryNode<number>>();
+  queue.enqueue(head);
+
+  while (queue.length) {
+    const curr = queue.deque();
+    if (!curr) {
+      continue;
+    }
+
+    if (curr.value === needle) {
+      return true;
+    }
+
+    if (curr.left) {
+      queue.enqueue(curr.left);
+    }
+
+    if (curr.right) {
+      queue.enqueue(curr.right);
+    }
+  }
+
+  return false;
 }
